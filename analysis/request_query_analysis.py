eee8b9862cb89f02da660311ca22ab0bca9ed5a2
Simon Koch <simkoc@postfach.it>
2016-04-04 11:47:58 +0200

    finished the issue query normalization and hashing now works and is preliminarily tested

--- analysis/request_query_analysis.py
+++ analysis/request_query_analysis.py
@@ -1,8 +1,7 @@
 import os
 import sys
-import hashlib
-import sqlparse
 import sqlite3 as lite
+import sql_query_normalization as normalizer
 
 state_change_keywords = ['DELETE','INSERT','DROP','SET','UPDATE']
 
@@ -31,64 +30,18 @@ def get_relevant_request_ids(sqlitedb_path,keywords):
 class Query:
     query_string=""
     query_type=""
-    parse_tree=""
 
     def __init__(self,query_string,query_type):
         self.query_type = query_type
         self.query_string = query_string
-        self.parse_tree = sqlparse.parse(query_string)
 
     def __str__(self):
         return self.query_string
 
-
-def remove_whitespaces(tree):
-    if tree.is_group():
-        tree.tokens = [remove_whitespaces(element) for element in tree.tokens if not element.is_whitespace()]
-
-    return tree
-
-
-def remove_rhs_values_sub(element):
-    if type(element) is sqlparse.sql.Comparison:
-        element.tokens.remove(element.right) #delete any value as this might be different to eq queries in diff context
-        return element
-
-    if type(element) is sqlparse.sql.Assignment:
-        raise NameError('Unexpected assignment operator SQL')
-
-    return element
     
-
-def remove_rhs_values(tree):
-
-    if tree.is_group():
-        #print tree.tokens
-        tree.tokens = [remove_rhs_values(element) for element in [remove_rhs_values_sub(element) for element in tree.tokens]]
-        return tree #this return just looks pretty
-
-    return tree
-
-        
-def order_alphabetically(tree):
-    if tree.is_group():
-        tree.tokens = sorted([order_alphabetically(element) for element in tree.tokens], key=lambda el: el.value)
-
-    return tree
-
-
-def normalize_query_syntax_tree(tree):
-    return order_alphabetically(remove_rhs_values(remove_whitespaces(tree)))
-    #return remove_rhs_values(remove_whitespaces(tree))
-    #return remove_rhs_values(tree)
-
-def normalized_query_hash(query_string):
-    hashlib.md5(normalize_query_syntax_tree(sqlparse.parse(query_string)[0]).__str__()).hexdigest()
-
-    
-def queries_equal_p(rhs,lhs):
+#def queries_equal_p(rhs,lhs):
     #print "comparing {0} with {1}".format(rhs,lhs)
-    return normalized_query_hash(rhs) == normalized_query_hash(lhs)
+#    return normalizer.normalized_query_hash(rhs) == normalizer.normalized_query_hash(lhs)
 
 
 class Request:
@@ -133,57 +86,26 @@ def generate_request_objects(request_id_dictionary,sqlitedb_path) :
     return requests
                             
 
-class Query_analysis_result:
-    query = None
-    request_ids = []
-
-    def __init__(self,query,request_ids):
-        self.query = query
-        self.request_ids  = request_ids
-
-    def __str__(self):
-        return "[RESULT QUERY:{0} REQUEST_IDS:{1}]".format(self.query.query_string,self.request_ids)
-
-
-#this stuff can (now) be done more efficient by just using a hashtable for the query categorization
-#but I am lazy!
-def split_query_list(query_list,ref_query):
-    neq = []
-    eq = []
-    for query in query_list:
-        if (query.query_type == ref_query.query_type and
-            queries_equal_p(query.query_string,ref_query.query_string)):
-            eq.append(query)
-        else:
-            neq.append(query)
-    return neq,eq
-
-
-def update_query_analysis_result_m(result,request):
-    neq_queries,eq_queries = split_query_list(request.queries,result.query)
-        
-    if len(neq_queries) == len(request.queries):
-        return result
-    else:
-        request.queries = neq_queries
-        result.request_ids.append(request.request_id)    
-                 
-
-def analyse_queries_of_requests_m(requests):
-    results = []
-    for idx,request in enumerate(requests):
+def analyse_queries_of_requests(requests):
+    queryhash_querystring_dict = {}
+    queryhash_request_dict = {}
+    for request in requests:
         for query in request.queries:
-            result = Query_analysis_result(query,[ request.request_id ])
-            for rem_request in requests[idx:]:
-                update_query_analysis_result_m(result,rem_request)
-            results.append(result)
+            query_hash = normalizer.generate_normalized_query_hash(query.query_string)
+            
+            if not query_hash in queryhash_querystring_dict:
+                queryhash_querystring_dict[query_hash] = query.query_string
+                queryhash_request_dict[query_hash] = [ request.request_id ]
+            else:
+                queryhash_request_dict[query_hash].append(request.request_id)
 
-    return results
+    return queryhash_querystring_dict,queryhash_request_dict
 
 
 def main(argv=sys.argv):
     requests = generate_request_objects(get_relevant_request_ids(argv[1],state_change_keywords),
                                         argv[1])
+    queryhash_querystring_dict,queryhash_request_dict = analyse_queries_of_requests(requests)
 
     print ""
     print "------- REQUESTS ------"
@@ -192,11 +114,19 @@ def main(argv=sys.argv):
         print request
 
     print ""
-    print "------- RESULTS ------"
+    print "------- QUERY-HASHS ------"
+
     
-    results = analyse_queries_of_requests_m(requests)
-    for result in results:
-        print result
+    for key,value in queryhash_querystring_dict.iteritems():
+        print "HASH {0}\nQUERY {1}\n".format(key,value)
+
+
+    print ""
+    print "------- QUERY-HASHS TO REQUESTS------"
+
+        
+    for key,value in queryhash_request_dict.iteritems():
+        print "HASH {0}\nREQUESTS {1}\n".format(key,value)
 
     
             
