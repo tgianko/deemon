549131948ecf7b0245d39b0feb82db0bd6362025
Simon Koch <s9sikoch@stud.uni-saarland.de>
2016-07-29 11:51:13 +0200

    fixed issue#16

--- vm-setup-scripts/polesno.sh
+++ vm-setup-scripts/polesno.sh
@@ -14,6 +14,23 @@ incoming_port='4444'
 outgoing_port='5555'
 com_port='7777'
 
+#inquire status of vm name 
+#exists -> delete
+#still runnin -> error
+
+
+
+if vboxmanage list vms | grep --quiet "\"$vm_name\""; then
+    if vboxmanage list runningvms | grep --quiet "\"$vm_name\""; then
+	echo "test vm $vm_name is currently running - shut down before trying again with using die .vdi and polesno.sh"
+	exit 1
+    else
+	vboxmanage unregistervm $vm_name
+    fi
+fi
+
+
+
 hostonlyifs_count=`vboxmanage list hostonlyifs | grep GUID | wc -l`
 if [ "$hostonlyifs_count" == "1" ]; then
     host_ip=`vboxmanage list hostonlyifs | grep IPAddress | sed 's/IPAddress:[ ]*//'`
