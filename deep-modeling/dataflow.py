6f64ac5d61d025a9746a95eb8a1339d57feb2e89
Simon Koch <s9sikoch@stud.uni-saarland.de>
2016-10-12 13:45:15 +0200

    fixed the datapropagation queries to extend to horizontal propagation for selenese to http request

--- deep-modeling/dataflow.py
+++ deep-modeling/dataflow.py
@@ -17,6 +17,21 @@ def get_data_flows(graph, rhs_data, rhs_start, lhs_start,
     return list(graph.run(query))
 
 
+def get_extended_data_flows_selenese_http(graph, rhs_data,
+                                          lhs_data, logger=None):
+    if logger is not None:
+        logger.info("selecting all extended {}->{} for Selenese->HTTPRequest"
+                    .format(lhs_data, rhs_data))
+
+    query = "MATCH (rhs:{})<-[rr*1..12]\
+-(h:HTTPRequest)<-[c:CAUSED]-(:SeleneseCommand)<-[:NEXT*0..100]-(s:SeleneseCommand)-[rl*1..12]\
+->(lhs:{}) WHERE NOT (s)-[:NEXT|CAUSED]->()-[*]->(lhs)\
+ AND NOT (h)-[:NEXT|CAUSED]->()-[*]->(rhs) AND\
+ rhs.value = lhs.value return lhs,rhs".format(rhs_data, lhs_data)
+
+    return list(graph.run(query))
+
+
 def insert_data_flow_between_pairs(graph, pairList, logger=None):
     if logger is not None:
         logger.info("inserting {} data flow pairs".format(len(pairList)))
@@ -28,21 +43,17 @@ def insert_data_flow_between_pairs(graph, pairList, logger=None):
 
 def insert_data_flow_selenese_HTTPRequest(graph, logger):
     insert_data_flow_between_pairs(
-        graph, get_data_flows(graph, "DataValue",
-                              "HTTPRequest", "SeleneseCommand",
-                              "DataValue", logger), logger)
+        graph, get_extended_data_flows_selenese_http(
+            graph, "DataValue", "DataValue", logger), logger)
     insert_data_flow_between_pairs(
-        graph, get_data_flows(graph, "KeyValuePair",
-                              "HTTPRequest", "SeleneseCommand",
-                              "DataValue", logger), logger)
+        graph, get_extended_data_flows_selenese_http(
+            graph, "DataValue", "KeyValuePair", logger), logger)
     insert_data_flow_between_pairs(
-        graph, get_data_flows(graph, "DataValue",
-                              "HTTPRequest", "SeleneseCommand",
-                              "KeyValuePair", logger), logger)
+        graph, get_extended_data_flows_selenese_http(
+            graph, "KeyValuePair", "DataValue", logger), logger)
     insert_data_flow_between_pairs(
-        graph, get_data_flows(graph, "KeyValuePair",
-                              "HTTPRequest", "SeleneseCommand",
-                              "KeyValuePair", logger), logger)
+        graph, get_extended_data_flows_selenese_http(
+            graph, "KeyValuePair", "KeyValuePair", logger), logger)
 
 
 def insert_data_flow_HTTPRequest_SQLQuery(graph, logger):
