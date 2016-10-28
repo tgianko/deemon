226517e96101f5cb753be0845174ac9b26ff7864
Giancarlo Pellegrino <gpellegrino@mmci.uni-saarland.de>
2016-10-28 17:31:46 +0200

    new dm_types

--- deep-modeling/api/dm_types.py
+++ deep-modeling/api/dm_types.py
@@ -1,10 +1,40 @@
-SELENESE   = "SelenseCommand"
+"""
+DEEP MODEL TYPES
+"""
+
+"""
+Selenium
+"""
+
+SELENESE   = "SeleneseCommand"
+
+"""
+HTTP-related
+"""
+
 URL        = "URL"
 HTTPREQ    = "HttpRequest"
 HTTPRESP   = "HttpResponse"
 COOKIE     = "Cookie"
 SETCOOKIE  = "SetCookie"
+
+"""
+Message encoding related
+"""
+
 MULTIPART  = "Multipart"
-SQLQEUERY  = "SQLQuery"
-ABSQUERY   = "AbsQuery"
+
+"""
+Server-side Program related
+"""
+XDEBUG     = "Xdebug"
 PHPSESSION = "PHPSession"
+
+"""
+Disk I/O related
+"""
+SQL        = "SQLQuery"
+ABSQUERY   = "AbsQuery"
+
+
+
