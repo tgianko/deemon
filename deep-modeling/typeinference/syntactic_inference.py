0267d1819abad51e63d26b069ffdf1e57cfa8dac
Florian Loch (D059349) <florian.loch@sap.com>
2016-11-18 12:27:50 +0100

    Split implementation in multiple files, add support for HEX and URL. Fix ugly bug in FSM

---
+++ deep-modeling/typeinference/syntactic_inference.py
@@ -0,0 +1,101 @@
+import types
+import url_regex
+import re
+
+STRING_TYPE = 0 
+INT_TYPE = 1
+FLOAT_TYPE = 2
+BOOL_TYPE = 3
+HEX_TYPE = 4
+UUID_TYPE = 5
+URL_TYPE = 6
+
+def infer_syntactic_type(values):
+    instancesOfTypesFound = [0]*7
+
+    for value in values:
+        assert type(value) == types.StringType, "All values have to be of type 'String'!"
+
+        guessedType = _infer_basic_type(value)
+
+        if guessedType == STRING_TYPE:
+            guessedType = _infer_advanced_type(value)
+
+        instancesOfTypesFound[guessedType] += 1 
+
+    # Check whether all values share the same type
+    for idx, counter in enumerate(instancesOfTypesFound):
+        if counter == len(values):
+            return idx
+
+    # If not, check for special case FLOAT_TYPE > INT_TYPE, else it has to be a STRING_TYPE
+    if instancesOfTypesFound[INT_TYPE] + instancesOfTypesFound[FLOAT_TYPE] == len(values):
+        return FLOAT_TYPE
+    elif instancesOfTypesFound[INT_TYPE] + instancesOfTypesFound[HEX_TYPE] == len(values):
+        return HEX_TYPE
+
+    return STRING_TYPE
+
+
+def _infer_basic_type(value):
+    dea = BasicTypeDEA()
+    
+    for c in value:
+        dea.digest(c)
+
+    return dea.get_state()
+
+
+def _infer_advanced_type(value):
+    lowered = value.lower()
+    # Bool type
+    if lowered in ["true", "false"]:
+        return BOOL_TYPE
+    
+    # HEX Type
+    if re.match(r"^([0-9]|[a-f])+$", lowered):
+        return HEX_TYPE
+
+    # URL type
+    if re.match(url_regex.URL_REGEX, value):
+        return URL_TYPE
+
+
+    # UUID Type
+
+    return STRING_TYPE
+
+class BasicTypeDEA:
+    START_STATE = 3
+    STRING_STATE = 0
+    INT_STATE = 1
+    FLOAT_STATE = 2
+
+    DIGIT = 0
+    DOT = 1
+    ANYTHING_ELSE = 2
+
+    DEA = [[STRING_STATE, STRING_STATE, STRING_STATE], [INT_STATE, FLOAT_STATE, STRING_STATE], [FLOAT_STATE, STRING_STATE, STRING_STATE], [INT_STATE, FLOAT_STATE, STRING_STATE]]
+
+    def __init__(self):
+        self._current_state = 3
+
+
+    def digest(self, c):
+        ascii_pos = ord(c)
+        transition = self.ANYTHING_ELSE # default case, gets overriden otherwise
+
+        if ascii_pos >= 48 and ascii_pos<= 57:
+            transition = self.DIGIT
+        elif c == ".":
+            transition = self.DOT
+        
+        self._current_state = self.DEA[self._current_state][transition]
+
+
+    def get_state(self):
+        if self._current_state == self.START_STATE:
+            return self.STRING_STATE
+
+        return self._current_state
+    
--- deep-modeling/typeinference/test_typeinference.py
+++ deep-modeling/typeinference/test_typeinference.py
@@ -3,18 +3,40 @@
 import unittest
 import typeinference
 
-class TestStringMethods(unittest.TestCase):
+class TestSyntacticInference(unittest.TestCase):
     BASIC_SAMPLES = [
         [["abc", "123", "1.2"], typeinference.STRING_TYPE],
         [["1", "2", "3"], typeinference.INT_TYPE],
         [["1", "123", "1.2"], typeinference.FLOAT_TYPE],
-        [["123", "123", "1.2."], typeinference.STRING_TYPE],        
+        [["123", "123", "1.2."], typeinference.STRING_TYPE],   
+        [[""], typeinference.STRING_TYPE],   
+        [[".123", "123", "1.2"], typeinference.FLOAT_TYPE],
+        [["0", "09"], typeinference.INT_TYPE],                                                  
+    ]
+
+    ADVANCED_SAMPLES = [
+        [["abc", "abc", "1..2"], typeinference.STRING_TYPE],
+        [["true", "false"], typeinference.BOOL_TYPE],
+        # [["1", "2", "3"], typeinference.INT_TYPE], # TODO UUID
+        [["a872fcd", "a", "0", "123"], typeinference.HEX_TYPE], # TODO HEX
+        [["https://www.google.de", "http://heise.de"], typeinference.URL_TYPE], # URL
+        [["www.google.de"], typeinference.STRING_TYPE], # An URL needs a protocol        
+        [[""], typeinference.STRING_TYPE]
     ]
 
     def test_basic_inference(self):
-        for test in self.BASIC_SAMPLES:
+        self._run_matrix_test(self.BASIC_SAMPLES)
+        
+
+    def test_advanced_inference(self):
+        self._run_matrix_test(self.ADVANCED_SAMPLES)
+
+
+    def _run_matrix_test(self, matrix):
+        for idx, test in enumerate(matrix):
             inferred_type = typeinference.infer_syntactic_type(test[0])
-            self.assertEqual(inferred_type, test[1])
+            self.assertEqual(inferred_type, test[1], "Assertion no. " + str(idx) + " resolved to type: " + str(inferred_type))
+
 
 if __name__ == '__main__':
     unittest.main()
