22133feefcb88783abdfc638c7cbba5b91eccb5d
Florian Loch (D059349) <florian.loch@sap.com>
2016-11-21 19:42:39 +0100

    Implement infer_semantic_type

---
+++ deep-modeling/typeinference/semantic_inference.py
@@ -0,0 +1,54 @@
+# Always the same value for one user, so actually user constant
+SEM_TYPE_USER_UNIQUE = 0
+SEM_TYPE_SESSION_UNIQUE = 1  # Value once per user
+SEM_TYPE_CONSTANT = 2  # All values are equal, for all users
+SEM_TYPE_UNCERTAIN = 3  # Anything else
+
+# Gets a list containing a maximum of 3 tupels each holding "user" and "value"
+def infer_semantic_type(tuples):
+    if len(tuples) == 0:
+        return SEM_TYPE_UNCERTAIN
+
+    last_val = tuples[0]["value"]
+    values_per_user = {}
+    permitted_duplicates = 0 # for user unique a value might occur multiple times with the same user key
+    all_values = set()
+    constant = True
+    for item in tuples:
+        val = item["value"]
+        user_key = item["user"]
+
+        # Check whether the value is constant
+        if val != last_val:
+            constant = False
+
+        # User unique
+        if user_key not in values_per_user:
+            values_per_user[user_key] = set()
+
+        # long version: if item.value not in all_values or item.value in
+        # all_values and item.value in values_per_user[item.user]:
+        if val not in all_values:
+            values_per_user[user_key].add(val)
+        elif val in values_per_user[user_key]:
+            permitted_duplicates += 1
+
+        # Session unique
+        print "Adding: " + val
+        all_values.add(val)
+
+    # When constant, it cannot be unique in any way
+    if constant:
+        return SEM_TYPE_CONSTANT
+
+    # Everything that is session unique is also user unique, therefore this
+    # has to be checked first
+    if len(all_values) == len(tuples):
+        return SEM_TYPE_SESSION_UNIQUE
+
+    # If it is user unique (occurs at least two time for one user)
+    if len(all_values) + permitted_duplicates == len(tuples):
+        return SEM_TYPE_USER_UNIQUE
+
+    # Example for uncertain case: (user1, a), (user1, b), (user2, b)
+    return SEM_TYPE_UNCERTAIN
--- deep-modeling/typeinference/test_typeinference.py
+++ deep-modeling/typeinference/test_typeinference.py
@@ -3,39 +3,59 @@
 import unittest
 import typeinference
 
+
 class TestSyntacticInference(unittest.TestCase):
     BASIC_SAMPLES = [
         [["abc", "123", "1.2"], typeinference.STRING_TYPE],
         [["1", "2", "3"], typeinference.INT_TYPE],
         [["1", "123", "1.2"], typeinference.FLOAT_TYPE],
-        [["123", "123", "1.2."], typeinference.STRING_TYPE],   
-        [[""], typeinference.STRING_TYPE],   
+        [["123", "123", "1.2."], typeinference.STRING_TYPE],
+        [[""], typeinference.STRING_TYPE],
         [[".123", "123", "1.2"], typeinference.FLOAT_TYPE],
-        [["0", "09"], typeinference.INT_TYPE],                                                  
+        [["0", "09"], typeinference.INT_TYPE],
     ]
 
     ADVANCED_SAMPLES = [
         [["abc", "abc", "1..2"], typeinference.STRING_TYPE],
         [["true", "false"], typeinference.BOOL_TYPE],
-        [["123e4567-e89b-12d3-a456-426655440000", "123e4567-e89b-12d3-a456-42665544abcd"], typeinference.UUID_TYPE], 
+        [["123e4567-e89b-12d3-a456-426655440000",
+            "123e4567-e89b-12d3-a456-42665544abcd"], typeinference.UUID_TYPE],
         [["a872fcd", "a", "0", "123"], typeinference.HEX_TYPE],
         [["https://www.google.de", "http://heise.de"], typeinference.URL_TYPE],
-        [["www.google.de"], typeinference.STRING_TYPE], # An URL needs a protocol        
-        [["", "123e4567-e89b-12d3-a456-42665544000"], typeinference.STRING_TYPE] # Last block is just 11 digits long
+        [["www.google.de"], typeinference.STRING_TYPE],  # An URL needs a protocol
+        # Last block is just 11 digits long
+        [["", "123e4567-e89b-12d3-a456-42665544000"], typeinference.STRING_TYPE]
     ]
 
     def test_basic_inference(self):
         self._run_matrix_test(self.BASIC_SAMPLES)
-        
 
     def test_advanced_inference(self):
         self._run_matrix_test(self.ADVANCED_SAMPLES)
 
-
     def _run_matrix_test(self, matrix):
         for idx, test in enumerate(matrix):
             inferred_type = typeinference.infer_syntactic_type(test[0])
-            self.assertEqual(inferred_type, test[1], "Assertion no. " + str(idx) + " resolved to type: " + str(inferred_type))
+            self.assertEqual(inferred_type, test[
+                             1], "Assertion no. " + str(idx) + " resolved to type: " + str(inferred_type))
+
+
+class TestSemanticInference(unittest.TestCase):
+    SAMPLES = [
+        [[{"user": "user1", "value": "a"}, {"user": "user2", "value": "a"}], typeinference.SEM_TYPE_CONSTANT],
+        [[{"user": "user1", "value": "a"}, {"user": "user2", "value": "a"}, {"user": "user2", "value": "b"}], typeinference.SEM_TYPE_UNCERTAIN],
+        [[{"user": "user1", "value": "a"}, {"user": "user2", "value": "c"}, {"user": "user2", "value": "b"}], typeinference.SEM_TYPE_SESSION_UNIQUE],
+        [[{"user": "user1", "value": "a"}, {"user": "user2", "value": "b"}, {"user": "user2", "value": "b"}], typeinference.SEM_TYPE_USER_UNIQUE]
+    ]
+
+    def test_semantic_inference(self):
+        self._run_matrix_test(self.SAMPLES)
+
+    def _run_matrix_test(self, matrix):
+        for idx, test in enumerate(matrix):
+            inferred_type = typeinference.infer_semantic_type(test[0])
+            self.assertEqual(inferred_type, test[
+                             1], "Assertion no. " + str(idx) + " resolved to type: " + str(inferred_type))
 
 
 if __name__ == '__main__':
