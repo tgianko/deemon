cbd6d8dbde4484b00c303aac6cc1888036c01247
Florian Loch (D059349) <florian.loch@sap.com>
2016-11-21 19:42:11 +0100

    Format code

--- deep-modeling/typeinference/syntactic_inference.py
+++ deep-modeling/typeinference/syntactic_inference.py
@@ -2,7 +2,7 @@ import types
 import url_regex
 import re
 
-STRING_TYPE = 0 
+STRING_TYPE = 0
 INT_TYPE = 1
 FLOAT_TYPE = 2
 BOOL_TYPE = 3
@@ -10,25 +10,28 @@ HEX_TYPE = 4
 UUID_TYPE = 5
 URL_TYPE = 6
 
+
 def infer_syntactic_type(values):
-    instancesOfTypesFound = [0]*7
+    instancesOfTypesFound = [0] * 7
 
     for value in values:
-        assert type(value) == types.StringType, "All values have to be of type 'String'!"
+        assert type(
+            value) == types.StringType, "All values have to be of type 'String'!"
 
         guessedType = _infer_basic_type(value)
 
         if guessedType == STRING_TYPE:
             guessedType = _infer_advanced_type(value)
 
-        instancesOfTypesFound[guessedType] += 1 
+        instancesOfTypesFound[guessedType] += 1
 
     # Check whether all values share the same type
     for idx, counter in enumerate(instancesOfTypesFound):
         if counter == len(values):
             return idx
 
-    # If not, check for special case FLOAT_TYPE > INT_TYPE, else it has to be a STRING_TYPE
+    # If not, check for special case FLOAT_TYPE > INT_TYPE, else it has to be
+    # a STRING_TYPE
     if instancesOfTypesFound[INT_TYPE] + instancesOfTypesFound[FLOAT_TYPE] == len(values):
         return FLOAT_TYPE
     elif instancesOfTypesFound[INT_TYPE] + instancesOfTypesFound[HEX_TYPE] == len(values):
@@ -39,7 +42,7 @@ def infer_syntactic_type(values):
 
 def _infer_basic_type(value):
     dea = BasicTypeDEA()
-    
+
     for c in value:
         dea.digest(c)
 
@@ -51,7 +54,7 @@ def _infer_advanced_type(value):
     # Bool type
     if lowered in ["true", "false"]:
         return BOOL_TYPE
-    
+
     # HEX Type
     if re.match(r"^([\d|[a-f])+$", lowered):
         return HEX_TYPE
@@ -62,9 +65,10 @@ def _infer_advanced_type(value):
 
     # URL type
     if re.match(url_regex.URL_REGEX, value):
-        return URL_TYPE    
+        return URL_TYPE
 
     return STRING_TYPE
+
 
 class BasicTypeDEA:
     START_STATE = 3
@@ -76,27 +80,25 @@ class BasicTypeDEA:
     DOT = 1
     ANYTHING_ELSE = 2
 
-    DEA = [[STRING_STATE, STRING_STATE, STRING_STATE], [INT_STATE, FLOAT_STATE, STRING_STATE], [FLOAT_STATE, STRING_STATE, STRING_STATE], [INT_STATE, FLOAT_STATE, STRING_STATE]]
+    DEA = [[STRING_STATE, STRING_STATE, STRING_STATE], [INT_STATE, FLOAT_STATE, STRING_STATE], [
+        FLOAT_STATE, STRING_STATE, STRING_STATE], [INT_STATE, FLOAT_STATE, STRING_STATE]]
 
     def __init__(self):
         self._current_state = 3
 
-
     def digest(self, c):
         ascii_pos = ord(c)
-        transition = self.ANYTHING_ELSE # default case, gets overriden otherwise
+        transition = self.ANYTHING_ELSE  # default case, gets overriden otherwise
 
-        if ascii_pos >= 48 and ascii_pos<= 57:
+        if ascii_pos >= 48 and ascii_pos <= 57:
             transition = self.DIGIT
         elif c == ".":
             transition = self.DOT
-        
-        self._current_state = self.DEA[self._current_state][transition]
 
+        self._current_state = self.DEA[self._current_state][transition]
 
     def get_state(self):
         if self._current_state == self.START_STATE:
             return self.STRING_STATE
 
         return self._current_state
-    
