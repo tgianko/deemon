ab419ebbf6dad72748e19d808aa0b48a01a6ce43
Simon Koch <s9sikoch@stud.uni-saarland.de>
2016-12-12 13:19:05 +0100

    changed minor details in database.lisp

--- mosgi/src/database.lisp
+++ mosgi/src/database.lisp
@@ -39,5 +39,4 @@
                                                                  (clsql:database-name database-connection))))))
 
 
-(with-open-file (test "/home/simkoc/xdebug.debug")
-  (FORMAT nil "~a" (pathname test)))
+
