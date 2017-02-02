b8292cc9bb4cfff72bb44ab097d5926588cb800e
Giancarlo Pellegrino <gpellegrino@mmci.uni-saarland.de>
2017-02-02 20:34:59 +0100

    bug in vilanoo. sometimes it terminates too early while there may be still requests to be written in the DB

--- vilanoo2/src/vilanoo2.py
+++ vilanoo2/src/vilanoo2.py
@@ -357,6 +357,7 @@ def start_selenese_runner(fname,selenese_log):
         else:
             s_logger.info("Selenese-runner-jar terminated with code {}. Sending SIGTERM.".format(proc.poll()))
 
+        time.sleep(args.wait*2)
         os.kill(os.getpid(), signal.SIGTERM)
 
     s_logger.info("Running selenese-runner.jar")
